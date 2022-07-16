e5aff7b2154c41ee5e2a080d60adc56acc424d40
Milind-Badekar <badekarmilind@gmail.com>
2022-07-16 21:12:46 +0530

    Adding files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import FileUpload from "../src/components/FileUpload";
+const App = () => (
+  <div className="container mt-4">
+    <h4 className="diplay-4 text-center mb-4">
+      <i className="fab fa-react"></i>React File upload
+    </h4>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <FileUpload />
+  </div>
+);
 
 export default App;
